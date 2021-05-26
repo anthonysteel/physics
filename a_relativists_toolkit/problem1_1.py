c3555a95a7146061ee83e5a1bf2c905d20a45499
Anthony Steel <13as261@queensu.ca>
2021-05-26 16:51:52 -0400

    Work on A Relativists Toolkit cone metric problem

--- a_relativists_toolkit/problem1_1.py
+++ a_relativists_toolkit/problem1_1.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def sphere_mesh(x0, y0, z0, r):
-    theta, phi = np.meshgrid(np.linspace(0, 2*np.pi, 50),\
-                              np.linspace(0, np.pi, 50))      
-    x = r * np.sin(phi) * np.cos(theta)
-    y = r * np.sin(phi) * np.sin(theta)
-    z = r * np.cos(phi)
+def cone_mesh(x0, y0, z0, r, alpha):
+    phi, r = np.meshgrid(np.linspace(0, 2*np.pi, 50),\
+                      np.linspace(0, r, 50))
+    x = r * np.sin(alpha) * np.cos(phi)
+    y = r * np.sin(alpha) * np.sin(phi)
+    z = r * np.cos(alpha)
     return x + x0, y + y0, z + z0
 
 
@@ -14,7 +14,9 @@ def main():
     fig = plt.figure()
     ax = plt.axes(projection="3d")
 
-    x, y, z = sphere_mesh(0, 0, 0, 1)
+
+    x, y, z = cone_mesh(0, 0, 0, 1, np.pi/16)
+    ax.set_box_aspect((np.ptp(x), np.ptp(y), np.ptp(z)))  # aspect ratio is 1:1:1 in data space
     ax.plot_surface(x, y, z)
     plt.show()
 
